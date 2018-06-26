0558d05260063025dc90cffdb38e0df8ae69b36e
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-26 02:30:13 +0000

    automatically backed up by learn

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -3,3 +3,15 @@ require 'rails_helper'
 RSpec.describe Student, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+describe Post do
+  it 'can be created' do
+    student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
+    expect(student).to be_valid
+  end
+end
+
+it 'has a summary' do
+  post = Post.create!(title: "My title", description: "The post description")
+  expect(post.post_summary).to eq("My title - The post description")
+end
