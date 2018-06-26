1627893212e8a0b054087a30ae3035a6335f36fb
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-26 02:30:41 +0000

    automatically backed up by learn

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Student, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
 
-describe Post do
+describe Student do
   it 'can be created' do
     student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
     expect(student).to be_valid
