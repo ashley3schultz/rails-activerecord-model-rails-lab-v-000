8589c36a39243e6d513fd54bfcb90a587a328d83
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-26 02:16:08 +0000

    automatically backed up by learn

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Student, type: :model do
   Student.new(first_name: "Daenerys", last_name: "Targaryen")
-  Student.all.last.to_s to equal "Daenerys Targaryen"
-  pending "add some examples to (or delete) #{__FILE__}"
+  expect Student.all.last.to_s to equal "Daenerys Targaryen"
+  #pending "add some examples to (or delete) #{__FILE__}"
 end
