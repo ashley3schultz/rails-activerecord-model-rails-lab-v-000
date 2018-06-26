252d107a475713ba60595ec8431f2af469d94227
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-26 02:32:04 +0000

    automatically backed up by learn

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Student, type: :model do
     student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
     expect(student).to be_valid
   end
+
+  it 'has a summary' do
+    post = Post.create!(title: "My title", description: "The post description")
+    expect(post.post_summary).to eq("My title - The post description")
+  end
+  
 end
+
+
