9f8d29ddbbc3f6928e9f3977d3867685bc241fbe
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-26 02:26:10 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
-class Student < ActiveRecord::Base
-
+class Student
+attr_accessor :first_name, :last_name
   def to_s
-    self.first_name + self.last_name
+    self.first_name + " " + self.last_name
   end
 
 end
